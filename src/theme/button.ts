f9bf097a480f0b930731d5391410808a5d5ce8fe
Arnav Chauhan <35425489+foxy17@users.noreply.github.com>
2024-04-21 13:59:06 +0530

    :lipstick: Style: Add button variants

--- src/theme/button.ts
+++ src/theme/button.ts
@@ -1,18 +1,69 @@
 import { defineStyle, defineStyleConfig } from '@chakra-ui/react';
 
-const brandPrimary = defineStyle({
-  background: 'orange.500',
-  color: 'white',
-  fontFamily: 'serif',
+const primary = defineStyle({
+  background: 'white',
+  color: 'neutral.900',
   fontWeight: 'normal',
+  fontSize: '14px',
+  lineHeight: '22px',
+  _hover: {
+    background: 'neutral.300',
+    color: 'neutral.900',
+  },
+  _active: {
+    background: 'neutral.400',
+    color: 'neutral.900',
+  },
+});
 
-  // let's also provide dark mode alternatives
-  _dark: {
-    background: 'orange.300',
-    color: 'orange.800',
+const purple = defineStyle({
+  background: 'pastel-purple.500',
+  color: 'white',
+  fontWeight: 'normal',
+  fontSize: '14px',
+  lineHeight: '22px',
+  _hover: {
+    background: 'pastel-purple.550',
+    color: 'white',
+  },
+  _active: {
+    background: 'pastel-purple.600',
+    color: 'white',
+  },
+});
+
+const danger = defineStyle({
+  background: 'danger.600',
+  color: 'white',
+  fontWeight: 'normal',
+  fontSize: '14px',
+  lineHeight: '22px',
+  _hover: {
+    background: 'danger.550',
+    color: 'white',
+  },
+  _active: {
+    background: 'danger.600',
+    color: 'white',
+  },
+});
+
+const success = defineStyle({
+  background: 'success.600',
+  color: 'white',
+  fontWeight: 'normal',
+  fontSize: '14px',
+  lineHeight: '22px',
+  _hover: {
+    background: 'success.550',
+    color: 'white',
+  },
+  _active: {
+    background: 'success.600',
+    color: 'white',
   },
 });
 
 export const buttonTheme = defineStyleConfig({
-  variants: { brandPrimary },
+  variants: { primary, purple, danger },
 });
