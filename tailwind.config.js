75414008a914c6c27b6c004aaee64087f87df568
foxy17 <carnav098@gmail.com>
2024-09-10 17:48:38 +0530

    feat: :sparkles: add magic UI
    Add magic UI cli

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,58 +1,37 @@
-// eslint-disable-next-line no-undef
-import { colors } from './src/theme/colors';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const defaultTheme = require('tailwindcss/defaultTheme');
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  darkMode: ['media', 'class'],
+  darkMode: ['class'],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  prefix: '',
   theme: {
-    fontFamily: {
-      sans: ['Lexend Deca', ...defaultTheme.fontFamily.sans],
-      serif: ['Manrope', 'serif'],
-      roboto: ['Roboto', 'sans-serif'],
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
     },
     extend: {
-      colors: {
-        ...colors,
-      },
-      borderRadius: {
-        xs: '0.125rem',
-        s: '0.1875rem',
-        m: '0.25rem',
-        l: '0.375rem',
-        xl: '0.5rem',
-        '100-percent': '100%',
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
-      },
-      boxShadow: {
-        card: '0px -4px 16px -8px rgba(0,0,0,0.4)',
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
       },
       animation: {
-        'meteor-effect': 'meteor 5s linear infinite',
-      },
-      keyframes: {
-        meteor: {
-          '0%': {
-            transform: 'rotate(215deg) translateX(0)',
-            opacity: '1',
-          },
-          '70%': {
-            opacity: '1',
-          },
-          '100%': {
-            transform: 'rotate(215deg) translateX(-500px)',
-            opacity: '0',
-          },
-        },
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
-    require('tailwindcss-animate'),
-  ],
+  plugins: [require('tailwindcss-animate')],
 };
