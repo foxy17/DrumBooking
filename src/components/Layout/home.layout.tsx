c2864997212e5d29cfb34bd5db7291aa3e273bc9
Arnav Chauhan <35425489+foxy17@users.noreply.github.com>
2025-04-03 14:20:14 +0530

    style: adjust z-index of header in HomeLayout for improved layering and visual hierarchy

--- src/components/Layout/home.layout.tsx
+++ src/components/Layout/home.layout.tsx
@@ -8,7 +8,7 @@ export const HomeLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
 
   return (
     <div className="flex flex-col h-[100dvh] w-screen overflow-hidden">
-      <div className="bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50 h-12 border-b border-zinc-800 shrink-0">
+      <div className="bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-2 h-12 border-b border-zinc-800 shrink-0">
         <div className="flex justify-between items-center px-6  h-full max-w-2xl mx-auto">
           <div className="flex items-center gap-4">
             <span className="text-xl font-cirka lowercase">{username}</span>
